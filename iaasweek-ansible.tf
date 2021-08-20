71bc2655dbb1c980fdef6ad721bf94833c72c26d
HailsonJunior <hailson.fsj@gmail.com>
2021-08-20 00:52:48 +0000

    Creating 2 instances with Terraform

--- iaasweek-ansible.tf
+++ iaasweek-ansible.tf
@@ -15,52 +15,51 @@ provider "aws" {
     profile = "Terraform"
 }
 
-resource "aws_instance" "IaC-Server" {
+resource "aws_instance" "IaC-Host1" {
     ami = "ami-019212a8baeffb0fa"
     instance_type = "t2.micro"
-    key_name = "IaC"
-    security_groups = ["${aws_security_group.IaC_group.name}"]
+    key_name = "${aws_key_pair.my_key.key_name}"
+    security_groups = ["${aws_security_group.allow_iac_ssh.name}"]
     tags = {
-      Name = "IaC Server"
+      Name = "IaC-Host1"
     }
 }
 
-resource "aws_instance" "host1" {
+resource "aws_instance" "IaC-Host2" {
     ami = "ami-019212a8baeffb0fa"
     instance_type = "t2.micro"
-    key_name = "IaC"
-    security_groups = ["${aws_security_group.IaC_group.name}"]
+    key_name = "${aws_key_pair.my_key.key_name}"
+    security_groups = ["${aws_security_group.allow_iac_ssh.name}"]
     tags = {
-      Name = "host1"
+      Name = "IaC-Host2"
     }
 }
 
-resource "aws_instance" "host2" {
-    ami = "ami-019212a8baeffb0fa"
-    instance_type = "t2.micro"
-    key_name = "IaC"
-    security_groups = ["${aws_security_group.IaC_group.name}"]
-    tags = {
-      Name = "host2"
-    }
+resource "aws_key_pair" "my_key" {
+    key_name = "my_key"
+    public_key = "${file("/home/hailson.junior/.ssh/id_rsa.pub")}"
 }
 
-resource "aws_security_group" "IaC_group" {
-    name = "Iac_group"
+resource "aws_security_group" "allow_iac_ssh" {
+    name = "allow_iac_ssh"
     ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["187.45.60.85/32"]
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["172.31.93.181/32"]
     }
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
+      from_port = 0
+      to_port = 0
+      protocol = -1
+      cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
-output "public_dns" {
-  value = "${aws_instance.IaC-Server.public_dns}"
+output "public_dns_host1" {
+  value = "${aws_instance.IaC-Host1.public_dns}"
+}
+
+output "public_dns_host2" {
+  value = "${aws_instance.IaC-Host2.public_dns}"
 }
