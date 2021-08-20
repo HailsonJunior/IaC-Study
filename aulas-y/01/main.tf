13f584c0eec0de22b5a184c95ef37204776371cd
HailsonJunior <hailson.fsj@gmail.com>
2021-08-20 21:50:38 +0000

    format tf

--- aulas-y/01/main.tf
+++ aulas-y/01/main.tf
@@ -1,28 +1,28 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.27"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
+  }
 
-    required_version = ">= 0.14.9"
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-    region = "us-east-1"
-    shared_credentials_file = "/home/hailson.junior/.aws/credentials"
-    profile = "Terraform"
+  region                  = "us-east-1"
+  shared_credentials_file = "/home/hailson.junior/.aws/credentials"
+  profile                 = "Terraform"
 }
 
 resource "aws_s3_bucket" "my-tf-test-bucket" {
-    bucket = "my-tf-test-bucket-hjunior"
-    acl = "private"
-    tags = {
-        Name = "My bucket"
-        Environment = "Dev"
-        Managedby = "Terraform"
-        Owner = "Hailson Junior"
-        UpdatedAt = "2021-08-20"
-    }
+  bucket = "my-tf-test-bucket-hjunior"
+  acl    = "private"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby   = "Terraform"
+    Owner       = "Hailson Junior"
+    UpdatedAt   = "2021-08-20"
+  }
 }
