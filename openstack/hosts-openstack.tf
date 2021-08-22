1b62d74849584f61ec0c152a373ac6d346496a34
HailsonJunior <hailson.fsj@gmail.com>
2021-08-22 10:58:05 +0000

    adjust hosts openstack

--- openstack/hosts-openstack.tf
+++ openstack/hosts-openstack.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "controller" {
     }
 
     ebs_block_device {
-        device_name = "/dev/sda"
+        device_name = "/dev/xvda"
         volume_size = "30"
         volume_type = "gp2"
         delete_on_termination = true
