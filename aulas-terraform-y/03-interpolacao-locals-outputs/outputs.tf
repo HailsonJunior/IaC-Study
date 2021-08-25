54663bd3060da78289050eefea03e85a8fad67ed
hailson_junior <hailson_junior@hst.com.br>
2021-08-25 17:44:52 -0300

    locals

---
+++ aulas-terraform-y/03-interpolacao-locals-outputs/outputs.tf
@@ -0,0 +1,13 @@
+output "bucket_name" {
+  value = aws_s3_bucket.hjbucket.arn
+  description = ""
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.hjbucket.bucker_domain_name
+}
+
+output "ips_file_path" {
+    value = "${aws_s3_bucket.hjbucket}/${aws_s3_bucket_object.hjbucket.key}"
+  
+}
--- aulas-terraform-y/03-interpolacao-locals-outputs/s3.tf
+++ aulas-terraform-y/03-interpolacao-locals-outputs/s3.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "hj-bucket" {
+resource "aws_s3_bucket" "hjbucket" {
     bucket = "${random_pet.bucket.id}-${var.environment}"
 
     tags = local.common_tags
 }
 
-resource "aws_s3_bucket_object" "hj-bucket" {
-    bucket = aws_s3_bucket.hj-bucket.bucket
+resource "aws_s3_bucket_object" "hjbucket" {
+    bucket = aws_s3_bucket.hjbucket.bucket
     key = "config/${local.ip_filepath}"
     source = local.ip_filepath
     etag = filemd5(local.ip_filepath)
