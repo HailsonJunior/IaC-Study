287193b673e3357ea02b0614f0f7a144494a5ae0
hailson_junior <hailson_junior@hst.com.br>
2021-08-25 17:06:29 -0300

    locals

--- aulas-terraform-y/03-interpolacao-locals-outputs/s3.tf
+++ aulas-terraform-y/03-interpolacao-locals-outputs/s3.tf
@@ -1,10 +1,12 @@
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "hj-bucket" {
     bucket = "${random_pet.bucket.id}-${var.environment}"
 
-    tags = {
-        Service = "Curso Terraform"
-        ManagedBy = "Terraform"
-        Environment = var.environment
-        Owner = "Hailson Junior"
-    }
+    tags = local.common_tags
 }
+
+resource "aws_s3_bucket_object" "hj-bucket" {
+    bucket = aws_s3_bucket.this.bucket
+    key = "config/${local.ip_filepath}"
+    source = local.ip_filepath
+    etag = filemd5(local.ip_filepath)
+    }
