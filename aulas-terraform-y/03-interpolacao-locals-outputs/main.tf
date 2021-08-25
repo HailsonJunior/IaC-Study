cbb93b283a6aa1575e2cc6fb2264c18c464069d4
HailsonJunior <hailson.fsj@gmail.com>
2021-08-25 19:42:25 +0000

    Corrections

--- aulas-terraform-y/03-interpolacao-locals-outputs/main.tf
+++ aulas-terraform-y/03-interpolacao-locals-outputs/main.tf
@@ -19,12 +19,3 @@ provider "aws" {
   shared_credentials_file = var.aws_credential
   profile                 = var.aws_profile
 }
-
-resource "aws_instance" "web" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Web"
-  }
-}
