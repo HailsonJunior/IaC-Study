360e433c9888beea7131d65f737dc0f62e92214e
hailson_junior <hailson_junior@hst.com.br>
2021-08-25 17:08:52 -0300

    locals

--- aulas-terraform-y/03-interpolacao-locals-outputs/s3.tf
+++ aulas-terraform-y/03-interpolacao-locals-outputs/s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "hj-bucket" {
 }
 
 resource "aws_s3_bucket_object" "hj-bucket" {
-    bucket = aws_s3_bucket.this.bucket
+    bucket = aws_s3_bucket.hj-bucket.bucket
     key = "config/${local.ip_filepath}"
     source = local.ip_filepath
     etag = filemd5(local.ip_filepath)
