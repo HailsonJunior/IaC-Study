9008a85cb95ce16aeddbaedc7903312ba64bf58d
hailson_junior <hailson_junior@hst.com.br>
2021-08-30 00:56:31 -0300

    IaaS Week Terraform

---
+++ IaaS-Week/terraform/routetable-private.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table" "private" {
+    count = var.az_count
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
+    }
+
+    tags = {
+      "Environment" = "iaasweek"
+    }
+}
