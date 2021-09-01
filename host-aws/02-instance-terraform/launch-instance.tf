e21c36293811c917a3afd651fa0b99cbbb1a6de6
HailsonJunior <hailson.fsj@gmail.com>
2021-09-01 18:50:19 +0000

    AWS host

--- host-aws/02-instance-terraform/launch-instance.tf
+++ host-aws/02-instance-terraform/launch-instance.tf
@@ -1,28 +1,28 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    shared_credentials_file = "./.aws/credentials"
-    profile = "hjunior"
+  region                  = "us-east-1"
+  shared_credentials_file = "./.aws/credentials"
+  profile                 = "hjunior"
 }
 
 resource "aws_instance" "aws-host" {
-    ami = "ami-0c59e532e98c72c0d"  
-    instance_type = "t2.micro"
-    key_name = "iac-new.pem"
-    security_groups = [ "iac-group" ]
-    tags = {
-      "Name" = "aws-study"
-    }
+  ami             = "ami-0c59e532e98c72c0d"
+  instance_type   = "t2.micro"
+  key_name        = "iac-new"
+  security_groups = ["iac-group"]
+  tags = {
+    "Name" = "aws-study"
+  }
 }
 
 output "aws_public_dns" {
-  value = "${aws_instance.aws-host.public_dns}"
+  value = aws_instance.aws-host.public_dns
 }
