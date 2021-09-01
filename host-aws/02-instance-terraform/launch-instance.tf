a740fbda9bb804fa2fba924c8da8daba1eb3f59a
Hailson Junior <79525895+HailsonJunior@users.noreply.github.com>
2021-09-01 15:15:47 -0300

    AWS terraform

---
+++ host-aws/02-instance-terraform/launch-instance.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    shared_credentials_file = "./.aws/credentials"
+    profile = "hjunior"
+}
+
+resource "aws_instance" "aws-host" {
+    ami = "ami-0c59e532e98c72c0d"  
+    instance_type = "t2.micro"
+    key_name = "iac-new.pem"
+    security_groups = [ "iac-group" ]
+}
+
+output "aws_public_dns" {
+  value = "${aws_instance.aws-host.public_dns}"
+}
